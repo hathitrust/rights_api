36dfb9a77f8e12310c0a8e8af4da89b7678cd56b
Brian "Moses" Hall <moseshll@umich.edu>
2025-07-17 16:39:15 -0400

    ETT-420 Update Database Credentials (#26)
    - Switch to standard `MARIADB_*` ENV to replace obsolete `DATA_API_*` keys.
    - Do away with connection string and simplify `Database` class in imitation of holdings-backend and others.
    - Use `connection_validator` extension with Sequel connection to allow reconnects. (ETT-430)

--- lib/rights_api/database.rb
+++ lib/rights_api/database.rb
@@ -4,43 +4,20 @@ require "sequel"
 
 module RightsAPI
   class Database
-    # .connect will take
-    #  * a full connection string (passed here OR in the environment
-    #    variable RIGHTS_API_DATABASE_CONNECTION_STRING)
-    #  * a set of named arguments, drawn from those passed in and the
-    #    environment. Arguments are those supported by Sequel.
-    #
-    # Environment variables are mapped as follows:
-    #
-    #   user: RIGHTS_API_DATABASE_USER
-    #   password: RIGHTS_API_DATABASE_PASSWORD
-    #   host: RIGHTS_API_DATABASE_HOST
-    #   port: RIGHTS_API_DATABASE_PORT
-    #   database: RIGHTS_API_DATABASE_DATABASE
-    #   adapter: RIGHTS_API_DATABASE_ADAPTER
-    def connect(connection_string = nil, **)
-      return @connection if @connection
+    attr_reader :connection
 
-      connection_string ||= ENV["RIGHTS_API_DATABASE_CONNECTION_STRING"]
-      if connection_string.nil?
-        db_args = gather_args(**)
-        Sequel.connect(**db_args)
-      else
-        Sequel.connect(connection_string)
-      end
-    end
-
-    private
-
-    def gather_args(**args)
-      %i[user password host port database adapter].each do |arg|
-        args[arg] ||= ENV["RIGHTS_API_DATABASE_#{arg.to_s.upcase}"]
-      end
-
-      args[:host] ||= "localhost"
-      args[:adapter] ||= :mysql2
-      args[:database] ||= "ht"
-      args
+    def initialize
+      @connection = Sequel.connect(
+        adapter: :mysql2,
+        user: ENV["MARIADB_HT_RO_USERNAME"],
+        password: ENV["MARIADB_HT_RO_PASSWORD"],
+        host: ENV["MARIADB_HT_RO_HOST"],
+        database: ENV["MARIADB_HT_RO_DATABASE"],
+        encoding: "utf8mb4"
+      )
+      # Check once every few seconds that we're actually connected and reconnect if necessary
+      @connection.extension(:connection_validator)
+      @connection.pool.connection_validation_timeout = 5
     end
   end
 end
